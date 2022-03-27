6c52c4cf28963b9db60c64cf73b9c42bcfff32db
loty <jylee04@trenbe.com>
2022-03-27 14:41:49 +0900

    first commit

---
+++ src/main/java/com/ljy/dddstudy/DddStudyApplication.java
@@ -0,0 +1,13 @@
+package com.ljy.dddstudy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DddStudyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DddStudyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ljy/dddstudy/DddStudyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ljy.dddstudy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DddStudyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
